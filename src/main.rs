4fcc013783716456bc230598e968d20f2d7a21f1
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-06-13 16:45:23 +0200

    let's struct it

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,47 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+#[derive(Debug, Eq)]
+struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value: value }
+    }
+
+    // pub fn value(&self) -> u32 {
+    //     self.value
+    // }
+}
+
+impl Ord for Guess {
+    fn cmp(&self, other: &Guess) -> Ordering {
+        self.value.cmp(&other.value)
+    }
+}
+
+impl PartialOrd for Guess {
+    fn partial_cmp(&self, other: &Guess) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Guess {
+    fn eq(&self, other: &Guess) -> bool {
+        self.value == other.value
+    }
+}
+
 fn main() {
     println!("Yo guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = Guess::new(rand::thread_rng().gen_range(1, 101));
 
     loop {
         println!("Please input your guess.");
@@ -18,17 +55,12 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: i32 = match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        if guess < 1 || guess > 100 {
-            println!("The secret number will be between 1 and 100");
-            continue;
-        }
-
-        println!("You guessed: {}", guess);
+        let guess = Guess::new(guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
